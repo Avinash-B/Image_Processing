a8af11f6197ffd1e929c183bee8ad4341c8a07d4
Avinash <boppudiavinash@gmail.com>
2018-01-16 21:46:29 +0530

    Created a code for changing dataset names

---
+++ file_rename.py
@@ -0,0 +1,11 @@
+#Code for converting names of file in a order for training the neural network
+import os,sys
+
+count = 1
+all = os.listdir(".")
+for one in all:
+	if one.startswith("img_"):
+		os.rename(one,str(count)+".jpg")
+		count+=1
+	else:
+		pass
