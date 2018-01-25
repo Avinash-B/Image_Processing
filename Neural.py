0a227dc5a1f783a9142fb856950f49d0ece5a11b
Avinash-B <boppudiavinash@gmail.com>
2018-01-25 20:21:42 +0530

    Created fully functional model :)

--- Neural.py
+++ Neural.py
@@ -16,7 +16,12 @@ import numpy as np
 import cv2
 import os,sys
 
-
+#Code to counter overflow in lambda operator
+def activation(inputa):
+    shapes = inputa.shape
+    for i in range(0,shapes[1]):
+        inputa[0,i] = float(1)/(float(1)+np.exp(-1*inputa[0,i]))
+    return inputa
 # In[3]:
 
 
@@ -27,15 +32,17 @@ class Neural:
         self.wh1i = wh1i
         self.wh2h1 = wh2h1
         self.woh2 = woh2
-        self.act = lambda x: 1/1+np.exp(x)
     def train(self,image,result):
         #Training the neural net
         inputh1 = np.dot(image,(self.wh1i).T)
-        outputh1 = self.act(inputh1)
+        inputh1 = inputh1/1000
+        outputh1 = activation(inputh1)
         inputh2 = np.dot(outputh1,(self.wh2h1).T)
-        outputh2 = self.act(inputh2)
+        inputh2 = inputh2/100
+        outputh2 = activation(inputh2)
         inputo = np.dot(outputh2,(self.woh2).T)
-        outputo = self.act(inputo)
+        inputo = inputo/10
+        outputo = activation(inputo)
         #Calculating errors and back propagation
         error = result - outputo
         erroh2 = np.dot(error,self.woh2)
@@ -46,15 +53,18 @@ class Neural:
         self.wh1i+=self.lr*errh1i*np.dot((outputh1*(np.ones([1,100],dtype = np.int)-outputh1)).T,image)
         self.wh2h1+= self.lr*errh2h1*np.dot((outputh2*(np.ones([1,50],dtype = np.int)-outputh2)).T,inputh1)
         self.woh2+= self.lr*erroh2*np.dot((outputo*(np.ones([1,10],dtype = np.int)-outputo)).T,inputh2)
-        
+        np.save("wh1i.npy",self.wh1i)
+        np.save("wh2h1.npy",self.wh2h1)
+        np.save("woh2.npy",self.woh2)
+
     def test(self,image):
         inputh1 = np.dot(image,(self.wh1i).T)
-        outputh1 = self.act(inputh1)
+        outputh1 = activation(inputh1)
         inputh2 = np.dot(outputh1,(self.wh2h1).T)
-        outputh2 = self.act(inputh2)
+        outputh2 = activation(inputh2)
         inputo = np.dot(outputh2,(self.woh2).T)
-        outputo = self.act(inputo)
-        print inputo
+        outputo = activation(inputo)
+        print outputo
 
 
 # In[4]:
@@ -92,6 +102,7 @@ if choice == 1:
             #Converting the image to 1*784
             image = np.reshape(image,(1,784),order = 'C')
             network.train(image,result)
+    print "Completed training your model"
 elif choice == 2:
     #Code for testing a particular image in the directory
     for i in range(1,351,1):
@@ -101,4 +112,3 @@ elif choice == 2:
         network.test(image)
 else:
     exit(0)
-
