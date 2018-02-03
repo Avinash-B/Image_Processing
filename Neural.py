1d5568f0672eb8a0a18ea12e75afd95ff58508c5
Avinash-B <boppudiavinash@gmail.com>
2018-02-03 16:00:47 +0530

    Solved NaN problem in Neural.py

--- Neural.py
+++ Neural.py
@@ -21,12 +21,12 @@ import os,sys
 def logarithm(inputa):
     shapes = inputa.shape
     for i in range(0,shapes[1]):
-        if inputa[0,i] <=0:
+        if inputa[0,i] <0:
             inputa[0,i] = -1*inputa[0,i]
             inputa[0,i] = math.log(inputa[0,i])
             inputa[0,i] = -1*inputa[0,i]
-        else:
-            inputa[0,i] = math.log(inputa[0,i])
+        elif inputa[0,i]>1:
+            inputa[0,i] = math.log(inputa[0,i])  
     return inputa
 #Code to counter overflow in lambda operator
 def activation(inputa):
@@ -48,6 +48,7 @@ class Neural:
         #Training the neural net
         inputh1 = np.dot(image,(self.wh1i).T)
         inputh1 = logarithm(inputh1)
+        # print inputh1
         outputh1 = activation(inputh1)
         inputh2 = np.dot(outputh1,(self.wh2h1).T)
         inputh2 = logarithm(inputh2)
@@ -63,12 +64,11 @@ class Neural:
         #Updating the weights of the network along with managing the weights to be confined to 0-1
         self.wh1i+=self.lr*errh1i*np.dot((outputh1*(np.ones([1,100],dtype = np.int)-outputh1)/inputh1).T,image)
         #Dividing the weights with the largest number in it
-        #If this fails comment these weight lines
-        #self.wh1i = self.wh1i/max(self.wh1i)
+        self.wh1i = self.wh1i/np.amax(self.wh1i)
         self.wh2h1+= self.lr*errh2h1*np.dot((outputh2*(np.ones([1,50],dtype = np.int)-outputh2)/inputh2).T,inputh1)
-        #self.wh2h1 = self.wh2h1/max(self.wh2h1)
+        self.wh2h1 = self.wh2h1/np.amax(self.wh2h1)
         self.woh2+= self.lr*erroh2*np.dot((outputo*(np.ones([1,10],dtype = np.int)-outputo)/inputo).T,inputh2)
-        #self.woh2 = self.woh2/max(self.woh2)
+        self.woh2 = self.woh2/np.amax(self.woh2)
         np.save("wh1i.npy",self.wh1i)
         np.save("wh2h1.npy",self.wh2h1)
         np.save("woh2.npy",self.woh2)
@@ -108,26 +108,26 @@ if flag==1:
     woh2 = np.load('woh2.npy')
 elif flag==0:
     execfile("init.py")
-network = Neural(0.01,wh1i,wh2h1,woh2)
+network = Neural(0.1,wh1i,wh2h1,woh2)
 if choice == 1:
     #Code for training all images in MNIST Directory
     count = 0
     for count in range(0,10,1):
-        result = np.zeros((1,10),int)
+        result = np.zeros((1,10),float)
         result[0,count] = 1
         for i in range(1,61,1):
-            image = np.array(cv2.imread("./Training/"+str(count)+"/"+str(i)+".jpg"),int)
+            image = np.array(cv2.imread("./Training/"+str(count)+"/"+str(i)+".jpg"),float)
             image = image[:,:,0]
             #Converting the image to 1*784
-            image = np.reshape(image,(1,784),order = 'C')
+            image = np.reshape(image,(1,784),order = 'C')/255.0
             network.train(image,result)
     print "Completed training your model"
 elif choice == 2:
     #Code for testing a particular image in the directory
     for i in range(1,351,1):
-        image = np.array(cv2.imread("./Testing/"+str(i)+".jpg"),int)
+        image = np.array(cv2.imread("./Testing/"+str(i)+".jpg"),float)
         image = image[:,:,0]
-        image = np.reshape(image,(1,784),order = 'C')
+        image = np.reshape(image,(1,784),order = 'C')/255.0
         network.test(image)
 else:
     exit(0)
--- init.py
+++ init.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-wh1i = np.random.rand(100,784)
-wh2h1 = np.random.rand(50,100)
-woh2 = np.random.rand(10,50)
+wh1i = np.random.uniform(-100.0,100.0,(100,784))
+wh2h1 = np.random.uniform(-100.0,100.0,(50,100))
+woh2 = np.random.uniform(-100.0,100.0,(10,50))
 
 np.save('wh1i',wh1i)
 np.save('wh2h1',wh2h1)
